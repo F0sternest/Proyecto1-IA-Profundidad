1cec6e89919b9a8e67e658cd11922d2a1eaa79f7
F0sternest <kevin.rod2026@gmail.com>
2023-05-25 19:35:36 -0500

    Añadiendo el arbol grafico

--- Nodo.py
+++
@@ -1,5 +0,0 @@
-class Nodo():
-    def __init__(self):
-        self.derecha = None
-        self.valor = None
-        self.izquierda = None
--- Profundidad.py
+++ Profundidad.py
@@ -1,35 +1,32 @@
-from Nodo import Nodo
+from binarytree import Node
+
+class Nodo:
+    def __init__(self, valor, left=None, right=None):
+        self.valor = valor  
+        self.izquierda = left  
+        self.derecha = right 
 
 # Creando los nodos
-raiz = Nodo()
-nodo37 = Nodo()
-nodo20 = Nodo()
-nodo24 = Nodo()
-nodo34 = Nodo()
-nodo50 = Nodo()
-nodo58 = Nodo()
-nodo78 = Nodo()
+raiz = Nodo(46)
+nodo37 = Nodo(37)
+nodo20 = Nodo(20)
+nodo24 = Nodo(24)
+nodo34 = Nodo(34)
+nodo50 = Nodo(50)
+nodo58 = Nodo(58)
+nodo78 = Nodo(78)
 
-# Dando valores a los nodos y estableciendo conexiones
-raiz.valor = 46
+# Estableciendo conexiones
 raiz.izquierda = nodo37
 raiz.derecha = nodo50
-nodo37.valor = 37
 nodo37.izquierda = nodo20
-nodo20.valor = 20
 nodo20.derecha = nodo24
-nodo24.valor = 24
 nodo24.derecha = nodo34
-nodo50.valor = 50
 nodo50.izquierda = nodo58
 nodo50.derecha = nodo78
 
-# Nodos Hojas
-nodo34.valor = 34
-nodo58.valor = 58
-nodo78.valor = 78
 
-
+# Algoritmo para la busqueda por Profundidad
 def busquedaProfundidad(nodo):
     if nodo is not None:
         if nodo.valor == 58:
@@ -44,5 +41,18 @@ def busquedaProfundidad(nodo):
     else:
         return False
 
-
+# Imprimiendo resultado
+print("Busqueda y Arbol:")
 busquedaProfundidad(raiz)
+
+# Creando el arbol grafico
+root = Node(46)
+root.left = Node(37)
+root.left.left = Node(20)
+root.left.left.right = Node(24)
+root.left.left.right.right = Node(34)
+root.right = Node(50)
+root.right.left = Node(58)
+root.right.right = Node(78)
+
+print(root)
